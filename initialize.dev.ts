110d55b0f27a035a1cfb628369bf862ed1708380
Sontranwakumo <sontran@wakumo.vn>
2024-07-26 17:16:19 +0700

    feat: add initialize.dev.ts and init-running execution

---
+++ initialize.dev.ts
@@ -0,0 +1,36 @@
+import { exec } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const runCommand = (command: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        reject(error);
+      }
+      if (stderr) {
+        console.error(`Stderr: ${stderr}`);
+        reject(new Error(stderr));
+      }
+      console.log(stdout);
+      resolve();
+    });
+  });
+};
+
+const createDatabase = async () => {
+    console.log("Create database, input database password");
+    const command = `psql -U ${process.env.POSTGRES_USER} -a -c "CREATE DATABASE ${process.env.POSTGRES_DB};"`;
+    await runCommand(command);
+};
+
+createDatabase();
+
+/**
+ * psql -U postgres -a \
+    -c "SELECT 'CREATE DATABASE testdb2'  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'testdb2');"
+ */
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,23 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import {ConfigService} from '@nestjs/config';
-    
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-    imports:[
-        TypeOrmModule.forRootAsync({
-            useFactory:(configs:ConfigService)=>({
-                type:'postgres',
-                host:configs.get("DB_HOST")||'localhost',
-                port:configs.get("DB_PORT")||5432,
-                username:'postgres',
-                database: configs.get("POSTGRES_DB")||'testdb',
-                password: configs.get("POSTGRES_PASSWORD")||'dhruv',
-                autoLoadEntities:true,
-                synchronize:configs.get("SYNCHRONIZE")||true
-            }),
-            inject:[ConfigService]
-        })
-    ]
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configs: ConfigService) => ({
+        type: 'postgres',
+        host: configs.get('DB_HOST') || 'localhost',
+        port: configs.get('DB_PORT') || 5432,
+        username: configs.get('POSTGRES_USER') || 'postgres',
+        database: configs.get('POSTGRES_DB') || 'testdb',
+        password: configs.get('POSTGRES_PASSWORD') || 'dhruv',
+        // autoLoadEntities:true,
+        // synchronize:configs.get("SYNCHRONIZE")||true
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class DatabaseModule {}
